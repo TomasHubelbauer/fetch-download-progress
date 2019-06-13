a37535929cbb9e9fb6768bc7863a01b002b61658
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-06-13 16:12:17 +0200

    Put together a demo which loads a large image from Unsplash as well as a local file

---
+++ index.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', async () => {
+  await demo('https://source.unsplash.com/random');
+  await demo('index.html');
+});
+
+async function demo(url) {
+  console.log(url);
+  const response = await fetch(url);
+  response.headers.forEach(console.log);
+
+  const total = Number(response.headers.get('content-length'));
+  let loaded = 0;
+
+  // Note that this does not work but looks much more elegant
+  // https://stackoverflow.com/a/47759960/2715716
+  // for await (const { length } of response.body.getReader()) {
+  //   loaded += length;
+  //   console.log(((loaded / total) * 100).toFixed(2), '%');
+  // }
+
+  const reader = response.body.getReader();
+  let result;
+  while (!(result = await reader.read()).done) {
+    loaded += result.value.length;
+    console.log(loaded, '/', total);
+  }
+}
